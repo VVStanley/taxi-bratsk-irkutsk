c335df82d01f464f99520c6e72eb1a9ef1237ec8
Stanley <Stanley_23@mail.ru>
2018-11-25 18:23:30 +0800

    add site

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ Route::get('/', 'IndexController@index')->name('index');
 Route::post('/', 'IndexController@post')->name('post');
 
 // Authentication Routes...
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::get('manatee', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
